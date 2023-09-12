7475a6a3d00b50172ca05a389cc4b15c93f026f2
Anush Rawat <anushra0701@gmail.com>
2023-09-12 14:20:06 +0530

    JSX added

--- App.js
+++ App.js
@@ -1,18 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Element
-const Title = () => (
+const title = () => (
   <h1 className="head" id="heading">
     Namaste React using JSX
   </h1>
 );
 
-// React Functional Component
-// Component Composition
 const HeadingComponent = () => (
   <div id="container">
-    <Title />
+    {title()}
     <h1 className="heading">Namaste React from functional component</h1>
   </div>
 );
