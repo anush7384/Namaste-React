86eec9206faf8808724b49c70bb41e3969104b55
Anush Rawat <anushra0701@gmail.com>
2023-09-18 11:55:45 +0530

    restaurant card added

--- App.js
+++ App.js
@@ -22,18 +22,42 @@ const Header = () => {
   );
 };
 
-const Body = () => {
-  return(
-    <div className="body">
-      <div></div>
+const styleCard = {
+  backgroundColor: "#f0f0f0",
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={styleCard}>
+      <img
+        className="res-logo"
+        alt="food-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ix0pwn3ztudojeakq0ug"
+      />
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
     </div>
-  )
-}
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
