1d9beabde98ab1afe2919bdaa74c6ef1256d38a1
Anush Rawat <anushra0701@gmail.com>
2023-09-21 11:23:32 +0530

    Header button added

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,15 +4,29 @@ import RestaurantCard from "./RestaurantCard";
 import { resList } from "../utils/mockData";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
-  useEffect(()=>{
-    console.log("useEffect called");
-    // fetchfunction
-  },[])
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  // const fetchData = async() =>{
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    const arrayOfCards = json.data.cards;
+    const restaurant_list = "restaurant_grid_listing";
 
-  // }
+    for (const cardObj of arrayOfCards) {
+      if (cardObj.card.card && cardObj.card.card.id === restaurant_list) {
+        const resData =
+          cardObj.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        setListOfRestaurants(resData);
+      }
+    }
+  };
+
+  if (listOfRestaurants.length === 0) return <h1>Loading....</h1>;
 
   return (
     <div className="body">
